102f4e4314a0007422c3450b3b01d15d3fdae513
stevecashworks <chigbustephennamdi@gmail.com>
2025-03-26 19:32:38 +0100

    route changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const Host="0.0.0.0"
 const server = express();
 server.use(cors());
 
@@ -17,6 +17,6 @@ server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-server.listen(5000, () => {
+server.listen(5000, Host, () => {
   console.log(`Server is listening on port 5000`);
 });
